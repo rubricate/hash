3043d7138c982ddd75cb75da6b63b299f3a1e943
Estefanio NS <estefanions@gmail.com>
2017-09-01 17:17:56 -0300

    example

---
+++ example/md5.php
@@ -0,0 +1,22 @@
+<?php 
+
+require dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+
+
+use Rubricate\Hash\CryptHash;
+use Rubricate\Hash\AlgorithmHash;
+use Rubricate\Hash\SaltHash;
+
+
+$h = new CryptHash(
+    
+     new AlgorithmHash('md5'),
+     new SaltHash('YOUR_SALT')
+
+);
+
+$password = 'your_secret_password';
+$result   = $h->getStr($password);
+
+var_dump($result);
+
